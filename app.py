2a49cb64c924797600658f492377321b3fbd6d98
RiyujinD <riyujin.danma@gmail.com>
2025-01-10 15:52:59 +0100

    implementation of card animation

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-from flask import Flask, request, jsonify, render_template, session, redirect, url_for
+from flask import Flask, make_response, request, jsonify, render_template, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
-import os
+#import os
 
 app = Flask(__name__)
 
+app.jinja_env.cache = {}
+
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+
+#@app.after_request
+#def after_request(response):
+#    """Ensure responses aren't cached"""
+#    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#    response.headers["Expires"] = 0
+#    response.headers["Pragma"] = "no-cache"
+#    return response
 
 
 @app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
+def no_cache(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
     return response
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #app.config["SESSION_PERMANENT"] = False
 #app.config["SESSION_TYPE"] = "filesystem"
