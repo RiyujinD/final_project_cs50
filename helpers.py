c8024bf7325a91040069523c568d0487985fbed7
RiyujinD <riyujin.danma@gmail.com>
2025-04-03 09:01:27 +0200

    adding helper files with decorator to check if user login in case add other conn than spotify Oauth

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from flask import redirect, render_template, url_for ,session
+from functools import wraps
+
+
+# Decorator function to check if user login
+def login_required(f):
+  @wraps(f)                                         # Preserve original function metadata (name, docstrings ext..)
+  def decorated_function(*args, **kwargs):          # Decorator function with any number of positional argument/ names arguments (1, 2 || brand=yamaha, model=ninja) 
+    if "is_authenticated" not in session:
+      return redirect(url_for("login"))      
+    return f(*args, **kwargs)                       # if already login primary function is being call with it arguments if any
+  return decorated_function
+    
+
