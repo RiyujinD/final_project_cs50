ca5f15839f70aee29dc5464d437544f040fb7c84
RiyujinD <riyujin.danma@gmail.com>
2025-04-04 13:22:19 +0200

    new file to store const variable

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+import base64
+from googleapiclient.discovery import build
+
+load_dotenv()
+
+# Load Spotify credentials 
+CLIENT_ID = os.getenv("CLIENT_ID") 
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")  
+REDIRECT_URI = os.getenv("REDIRECT_URI") # Redirect URI Spotify OAuth flow
+AUTHORIZATION_URL = "https://accounts.spotify.com/authorize" 
+TOKEN_URL = "https://accounts.spotify.com/api/token" # Spotify token endpoint
+
+# Base64 encoding of the client ID and secret
+auth_str = f"{CLIENT_ID}:{CLIENT_SECRET}"                 
+auth_bytes = auth_str.encode("utf-8")                       # Convert to bytes
+auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")  # Base64 encode and decode to string
+
+# For tokens
+SPOTIFY_TOKEN_HEADERS = {
+    "Authorization": "Basic " + auth_base64,
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+# Load YouTube API Key from environment variables
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY) # Initialize YouTube API client
