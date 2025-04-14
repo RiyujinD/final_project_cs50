abe3e6bc18320ffd6f71fddfeefa85f5ef76c88a
RiyujinD <riyujin.danma@gmail.com>
2025-04-14 11:35:52 +0200

    New function to store unique tracks in database

--- helpersDB.py
+++ helpersDB.py
@@ -1,10 +1,12 @@
 import sqlite3
 from config import DATABASE
+from flask import session
 
 ### REMINDER TO ALWAYS CLOSE DB AFTER COMMITS ### 
 
 def link_db():
     db = sqlite3.connect(DATABASE)
+    db.execute("PRAGMA foreign_keys = ON")
     db.row_factory = sqlite3.Row  # Allows accessing columns by name e.g., row["id"]
     return db
 
@@ -26,6 +28,12 @@ def insert_userID(spotify_id):
 
 
 def insert_tracks_artists(unique_items):
+    
+    spotify_id = session["spotify_id"]
+    if not spotify_id:
+      print("No user ID found in session")
+      return
+
     try:
         db = link_db()
         cursor = db.cursor()
@@ -41,15 +49,21 @@ def insert_tracks_artists(unique_items):
             artists = ', '.join(artist_names)
 
             cursor.execute("""
-                INSERT OR IGNORE INTO track (track_id, track_name, duration_ms, artist_name)
+                INSERT OR IGNORE INTO tracks (track_id, track_name, duration_ms, artist_name)
                 VALUES (?, ?, ?, ?)
                 """, (track_id, name, duration, artists))
+
+            cursor.execute("""
+                INSERT OR IGNORE INTO user_tracks (spotify_user_id, track_id) 
+                VALUES (?, ?)
+                """, (spotify_id, track_id))
 
         db.commit()
 
     except Exception as e:
         print(f"Error inserting tracks and artists: {e}")
         db.rollback()
+
     finally:
         cursor.close()  
         db.close()
